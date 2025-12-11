45767672b7c72d830f2ee84cfc60ed07986de981
caddellw <wesley.l.caddell@gmail.com>
2025-12-11 01:29:07 -0500

    Updated UserID system to redirect to start a new session if the current one is invalid

--- server.js
+++ server.js
@@ -19,20 +19,26 @@ app.use('/images', express.static(path.join(__dirname, "uploads")))
 app.use(parser.json())
 
 app.get("/security/lostAndFound", function(req, res) {
-    let uid = req.query.userid
+    let userid = req.query.userid
 
     let redirect = false;
-    if ( !uid ) {
-        uid = uuidv4()
-        users.push(uid)
+
+    if ( !userid ) {
+        userid = uuidv4()
+        users.push(userid)
         
-        console.log(`User Experience Started: ${uid}`)
+        console.log(`User Experience Started: ${userid}`)
 
         redirect = true
     }
 
     if ( redirect ) {
-        res.redirect(`/security/lostAndFound?userid=${uid}`)
+        res.redirect(`/security/lostAndFound?userid=${userid}`)
+        return;
+    }
+
+    if ( !(users.includes(userid)) ) { // Invalid User ID
+        res.redirect(`/security/lostAndFound`)
         return;
     }
 
@@ -49,8 +55,8 @@ app.get("/security/itemCatalog", function(req, res) {
     let parameters = ["description", "type", "additionalNotes"]
     let displayCatalog = []
     
-    if ( !(users.includes(userid)) ) { //switch "includes" for "in" if we change to an object
-        res.send(JSON.stringify(packet))
+    if ( !(users.includes(userid)) ) { // Invalid User ID
+        res.redirect(`/security/lostAndFound`)
         return;
     }
 
@@ -80,8 +86,11 @@ app.get("/security/itemCatalog", function(req, res) {
     res.send(JSON.stringify(packet))
 })
 app.get("/security/request", function(req, res) {
-    let uid = req.query.userid
-    // ADD USER id validation
+    let userid = req.query.userid
+    if ( !(users.includes(userid)) ) { // Invalid User ID
+        res.redirect(`/security/lostAndFound`)
+        return;
+    }
 
     res.sendFile(path.join(__dirname, "security", "request.html"))
 })
@@ -92,8 +101,8 @@ app.get("/security/requestCatalog", function(req, res) {
     }
     let userid = req.query.userid
 
-    if ( !(users.includes(userid)) ) {
-        res.send(JSON.stringify(packet))
+    if ( !(users.includes(userid)) ) { // Invalid User ID
+        res.redirect(`/security/lostAndFound`)
         return;
     }
 
@@ -103,7 +112,11 @@ app.get("/security/requestCatalog", function(req, res) {
     res.send(JSON.stringify(packet))
 })
 app.get("/security/requestCatalogPage", function(req, res) {
-    let uid = req.query.userid
+    let userid = req.query.userid
+    if ( !(users.includes(userid)) ) { // Invalid User ID
+        res.redirect(`/security/lostAndFound`)
+        return;
+    }
 
     res.sendFile(path.join(__dirname, "security", "requestCatalog.html"))
 })
@@ -113,12 +126,11 @@ app.post("/security/request", upload.single('photo'), function(req, res) {
         message: 'Invalid User ID'
     }
     let userid = req.query.userid
-    // if ( !(userid in users) ) {
-    //     res.send(JSON.stringify(packet))
-    //     return;
-    // }
+    if ( !(users.includes(userid)) ) { // Invalid User ID
+        res.redirect(`/security/lostAndFound`)
+        return;
+    }
 
-    // let user = users[userid]
     let contactName = req.body.contactName
     let contactEmail = req.body.contactEmail
     let description = req.body.description
@@ -126,6 +138,11 @@ app.post("/security/request", upload.single('photo'), function(req, res) {
     let additionalNotes = req.body.additionalNotes
     let objectType = req.body.objectType
     let photo = req.file.filename
+    let mimetype = req.file.mimetype
+    if (mimetype != "image/png" || mimetype != "image/jpeg" || mimetype != "image/jpg") {
+        res.redirect(`/security/request?userid=${userid}`)
+        return;
+    }
     let item = [description, objectType, estimatedDateLost, additionalNotes, photo]
     let request = [[contactName, contactEmail], item]
 
@@ -137,7 +154,11 @@ app.post("/security/request", upload.single('photo'), function(req, res) {
 })
 app.get("/security/submit", function(req, res) {
     let userid = req.query.userid
-    //ADD USER id validation
+    console.log(users)
+    if ( !(users.includes(userid)) ) { // Invalid User ID
+        res.redirect(`/security/lostAndFound`)
+        return;
+    }
     
     res.sendFile(path.join(__dirname, "security", "submit.html"))
 })
@@ -148,10 +169,19 @@ app.post("/security/submit", upload.single('photo'), function(req, res) {
     }
     //Check issues in input
     let userid = req.query.userid
+    if ( !(users.includes(userid)) ) { // Invalid User ID
+        res.redirect(`/security/lostAndFound`)
+        return;
+    }
     let description = req.body.description
     let objectType = req.body.objectType
     let additionalNotes = req.body.additionalNotes
     let photo = req.file.filename
+    let mimetype = req.file.mimetype
+    if (mimetype != "image/png" || mimetype != "image/jpeg" || mimetype != "image/jpg") {
+        res.redirect(`/security/submit?userid=${userid}`)
+        return;
+    }
     let item = [description, objectType, additionalNotes, photo]
     
     itemCatalog.push(item)
@@ -161,7 +191,10 @@ app.post("/security/submit", upload.single('photo'), function(req, res) {
 })
 app.get("/security/authors", function(req, res) {
     let userid = req.query.userid
-    //ADD USER id validation
+    if ( !(users.includes(userid)) ) {
+        res.redirect(`security/lostAndFound`)
+        return;
+    }
 
     res.sendFile(path.join(__dirname, "security", "authors.html"))
 })
